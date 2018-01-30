cce07017872fd48731c837da3a9caad67ea6b772
Tsveta <2254765p@student.gla.ac.uk>
2018-01-30 12:32:29 +0000

    Built pygame skeleton

---
+++ snake.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Initialise pygame
+pygame.init()
+
+# Create window
+gameDisplay = pygame.display.set_mode((800,600))
+
+# Update (flip) display
+pygame.display.update()
+
+# Uninitialise and quit pyga
+pygame.quit()
+quit()
